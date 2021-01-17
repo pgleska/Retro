f98e18f7a855fd978746f2a5174f2c1b74dad09f
Piotr <piotr.gleska@wp.pl>
2021-01-17 22:35:29 +0100

    empty app

--- app/src/main/java/com/putnet/retro/MainActivity.java
+++ app/src/main/java/com/putnet/retro/MainActivity.java
@@ -1,11 +1,7 @@
 package com.putnet.retro;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -32,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home)
+                R.id.nav_all)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
